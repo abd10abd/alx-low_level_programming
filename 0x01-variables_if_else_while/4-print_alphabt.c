9c14f529a9359d5fb234947e224cd348377f0a40
Abdulkudus Abdushukur <abd0819hussen@gmail.com>
2022-06-06 16:12:50 -0700

    ./5-print_numbers was created

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -3,11 +3,11 @@
  * main - main block
  * Description: Use `putchar` to print all letters but the letter 'q' and 'e'.
  * Return: 0
- *  */
+ */
 int main(void)
 {
 	char c = 'a';
-	
+
 	while (c <= 'z')
 	{
 		if (c != 'q' && c != 'e')
